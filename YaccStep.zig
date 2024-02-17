aab78d6bd30004e002361dc5e66c090719bb88a7
Tristan Ross <tristan.ross@midstall.com>
2024-02-16 18:12:13 -0800

    feat(yacc): add prefix option

--- YaccStep.zig
+++ YaccStep.zig
@@ -2,28 +2,36 @@ const std = @import("std");
 const YaccStep = @This();
 const evalChildProcess = @import("build.zig").evalChildProcessInDirectory;
 
+pub const Options = struct {
+    source: std.Build.LazyPath,
+    isCpp: bool = false,
+    prefix: ?[]const u8 = null,
+};
+
 step: std.Build.Step,
 source: std.Build.LazyPath,
 isCpp: bool,
+prefix: ?[]const u8,
 output_source: std.Build.GeneratedFile,
 output_header: std.Build.GeneratedFile,
 
-pub fn create(b: *std.Build, source: std.Build.LazyPath, isCpp: bool) *YaccStep {
+pub fn create(b: *std.Build, options: Options) *YaccStep {
     const self = b.allocator.create(YaccStep) catch @panic("OOM");
     self.* = .{
         .step = std.Build.Step.init(.{
             .id = .custom,
-            .name = b.fmt("Yacc {s}", .{source.getDisplayName()}),
+            .name = b.fmt("Yacc {s}", .{options.source.getDisplayName()}),
             .owner = b,
             .makeFn = make,
         }),
-        .source = source,
-        .isCpp = isCpp,
+        .source = options.source,
+        .isCpp = options.isCpp,
+        .prefix = options.prefix,
         .output_source = .{ .step = &self.step },
         .output_header = .{ .step = &self.step },
     };
 
-    source.addStepDependencies(&self.step);
+    options.source.addStepDependencies(&self.step);
     return self;
 }
 
@@ -59,14 +67,23 @@ fn make(step: *std.Build.Step, _: *std.Progress.Node) anyerror!void {
 
     const cmd = try b.findProgram(&.{"yacc"}, &.{});
 
-    try evalChildProcess(step, &.{
+    var args = std.ArrayList([]const u8).init(b.allocator);
+    defer args.deinit();
+
+    try args.appendSlice(&.{
         cmd,
         self.source.getPath2(b, step),
         "-o",
         b.fmt("{s}.{s}", .{ name, sourceExt }),
         "-H",
         "-Wno-yacc",
-    }, try b.cache_root.join(b.allocator, &.{ "o", &digest }));
+    });
+
+    if (self.prefix) |prefix| {
+        try args.appendSlice(&.{ "-p", prefix });
+    }
+
+    try evalChildProcess(step, args.items, try b.cache_root.join(b.allocator, &.{ "o", &digest }));
 
     self.output_source.path = try b.cache_root.join(b.allocator, &.{ "o", &digest, b.fmt("{s}.{s}", .{ name, sourceExt }) });
     self.output_header.path = try b.cache_root.join(b.allocator, &.{ "o", &digest, b.fmt("{s}.{s}", .{ name, headerExt }) });
